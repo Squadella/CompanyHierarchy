b85bc825652cae87a64a987fe91a1748229c8c10
Squadella <pbouniol@et.esiea.fr>
2017-01-18 16:28:42 +0100

    First commit.

---
+++ src/MainWindow.java
@@ -0,0 +1,10 @@
+/**
+ * Created by Sauce Ultra on 18/01/2017.
+ */
+public class MainWindow
+{
+    public static void main (String[] args)
+    {
+        System.out.println("Hello World");
+    }
+}
