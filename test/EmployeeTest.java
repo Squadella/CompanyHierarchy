6d0ef5535c615bc48852c81b4e95777a67421cb9
Squadella <pbouniol@et.esiea.fr>
2017-01-19 11:07:06 +0100

    Fixed unit tests.

---
+++ test/EmployeeTest.java
@@ -0,0 +1,65 @@
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+/**
+ * Created by Sauce Ultra on 19/01/2017.
+ */
+class EmployeeTest {
+    @BeforeEach
+    void setUp() {
+
+    }
+
+    @Test
+    void getSalary() {
+
+    }
+
+    @Test
+    void getDepartment() {
+
+    }
+
+    @Test
+    void getPosition() {
+
+    }
+
+    @Test
+    void getSubEmployee() {
+
+    }
+
+    @Test
+    void removeEmployee() {
+
+    }
+
+    @Test
+    void getSirName() {
+
+    }
+
+    @Test
+    void getFirstName() {
+
+    }
+
+    @Test
+    void addSubordinate() {
+
+    }
+
+    @Test
+    void removeSubordinate() {
+
+    }
+
+    @Test
+    void setNewSupervisor() {
+
+    }
+
+}
