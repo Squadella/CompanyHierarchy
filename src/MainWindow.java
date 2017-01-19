5eaa880cd694a9d11fbbb55ebacaec0e2dba9d1e
Arthur <arthur.beaulieu@free.fr>
2017-01-19 11:27:46 +0100

    Add communication between .fxml and code (only for File > Quit so far)

--- src/MainWindow.java
+++ src/MainWindow.java
@@ -6,19 +6,35 @@ import javafx.stage.Stage;
 
 public class MainWindow extends Application
 {
+    //Attributes
+    private Stage _stage;
+
     public static void main(String[] args)
     {
         Application.launch(args);
     }
 
+    //Startup function, builds the interface from resources
     public void start(Stage stage) throws Exception
     {
-        Parent root = FXMLLoader.load(getClass().getResource("/res/CompanyHierarchyUI.fxml"));
+        //Loading .fxml from /src/res and setting FXML loader
+        FXMLLoader loader = new FXMLLoader();
+        loader.setLocation(getClass().getResource("/res/CompanyHierarchyUI.fxml"));
+        loader.setController(this);
+        //Creating scene from `loader`
+        Parent root = loader.load();
         Scene scene = new Scene(root, 1100, 515);
+        //Stage manipulation
+        _stage = stage;
+        _stage.setTitle("Company Hierarchy Manager");
+        _stage.setScene(scene);
+        _stage.setResizable(false);
+        _stage.show();
+    }
 
-        stage.setTitle("Company Hierarchy Manager");
-        stage.setScene(scene);
-        stage.setResizable(false);
-        stage.show();
+    //Close application
+    public void stop() throws Exception
+    {
+        _stage.close();
     }
 }
