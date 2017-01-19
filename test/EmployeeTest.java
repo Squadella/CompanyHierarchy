bc334edff660422ffd260f4567738d028bbc2493
zkclown <phamle@et.esiea.fr>
2017-01-19 11:14:59 +0100

    Implementation tests getters

--- test/EmployeeTest.java
+++ test/EmployeeTest.java
@@ -7,29 +7,31 @@ import static org.junit.jupiter.api.Assertions.*;
  * Created by Sauce Ultra on 19/01/2017.
  */
 class EmployeeTest {
+    Employee test;
     @BeforeEach
-    void setUp() {
-
+    public void initialize()
+    {
+        this.test = new Employee("CEO", "FirstNameTest", "SirNameTest","Marketing",0);
     }
-
     @Test
     void getSalary() {
-
+        assertEquals(0, test.getSalary());
     }
 
     @Test
     void getDepartment() {
-
+        assertEquals("Marketing", test.getDepartment());
     }
 
     @Test
     void getPosition() {
+        assertEquals("CEO", test.getPosition());
 
     }
 
     @Test
     void getSubEmployee() {
-
+        
     }
 
     @Test
@@ -39,12 +41,13 @@ class EmployeeTest {
 
     @Test
     void getSirName() {
+        assertEquals("SirNameTest", test.getSirName());
 
     }
 
     @Test
     void getFirstName() {
-
+        assertEquals("FirstNameTest", test.getFirstName());
     }
 
     @Test
