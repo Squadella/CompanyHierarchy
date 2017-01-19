18da7f94749f8e4a96eec93406cf34cf63f68adc
Arthur <arthur.beaulieu@free.fr>
2017-01-19 22:49:55 +0100

    Add ids and setter in controller

--- src/MainWindow.java
+++ src/MainWindow.java
@@ -10,6 +10,7 @@ public class MainWindow extends Application
 {
     //Attributes
     private Stage _stage;
+    private FXController _controller;
 
     public static void main(String[] args)
     {
@@ -19,27 +20,18 @@ public class MainWindow extends Application
     //Startup function, builds the interface from resources
     public void start(Stage stage) throws Exception
     {
-        //Loading .fxml from /src/res and setting FXML loader
+        //Loading .fxml from /src/res and setting FXML loader (w/ controller)
         FXMLLoader loader = new FXMLLoader(getClass().getResource("res/CompanyHierarchyUI.fxml"));
         loader.setLocation(MainWindow.class.getResource("res/CompanyHierarchyUI.fxml"));
         Parent root = loader.load();
-        //Creating scene from `loader`
-        //Parent root = loader.load();
         Scene scene = new Scene(root, 1100, 515);
-        FXController controller = loader.getController();
-        controller.changeText("HELLO WORLD!");
+        _controller = loader.getController();
+
         //Stage manipulation
         _stage = stage;
         _stage.setTitle("Company Hierarchy Manager");
         _stage.setScene(scene);
         _stage.setResizable(false);
         _stage.show();
-
-    }
-
-    //Close application
-    public void stop() throws Exception
-    {
-        _stage.close();
     }
 }
