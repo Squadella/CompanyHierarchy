f14831552d2f0d591ed7851e3597ccf40dd96ea8
zkclown <phamle@et.esiea.fr>
2017-01-19 09:24:25 +0100

    Added Junit.

---
+++ src/EmployeeTest.java
@@ -0,0 +1,14 @@
+import org.junit.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+/**
+ * Created by zk on 19/01/17.
+ */
+class EmployeeTest {
+    @Test
+    void getSirName() {
+        System.out.println("zob");
+    }
+
+}
